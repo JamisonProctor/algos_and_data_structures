06cc1ce1732531d8bf126ea311175824b9a4c70b
mr.mitternacht <jamison.proctor@gmail.com>
2023-10-19 16:52:13 +0200

    add arrays.py

---
+++ arrays.py
@@ -0,0 +1,27 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list:
+    print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+    break
+
+numbers = []
+print(len(numbers))
+
+numbers.append(2)
+print(len(numbers))
+
+numbers.append(200)
+print(len(numbers))
+
+numbers = []
+numbers.extend([4, 5, 6])
+print(numbers)
+
+numbers.insert(0, 3)
+print(numbers)  
+
