9c1751f6052447e1de246f4a9da86a97088aa510
mr.mitternacht <jamison.proctor@gmail.com>
2023-10-25 09:50:09 +0200

    adjusted code in list_generator.py to reduce the chance of recurtion depth errors when sorting.

--- list_generator.py
+++ list_generator.py
@@ -1,5 +1,5 @@
-import random
+from random import randrange
 
-with open("numbers/10000.txt", "w") as f:
-    for i in range(10000):
-        f.write(str(random.randint(0, 10000)) + "\n")
+with open("numbers/1000000.txt", "w") as f:
+    for i in range(10000000):
+        f.write(str(randrange(0, 1000000)) + "\n")
