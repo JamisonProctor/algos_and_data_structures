59b45ba33a4ab988f5f76cf3d90cc61145d9e8f1
mr.mitternacht <jamison.proctor@gmail.com>
2023-10-25 10:16:57 +0200

    add name_list_generator.py which generates lists of random names for sorting.

---
+++ name_list_generator.py
@@ -0,0 +1,22 @@
+import random
+
+# Sample list of first names
+first_names = [
+    "Alice", "Bob", "Charlie", "David", "Emily", "Fiona",
+    "George", "Helen", "Ivan", "Jill", "Kevin", "Laura"
+]
+
+# Sample list of last names
+last_names = [
+    "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller",
+    "Davis", "Garcia", "Rodriguez", "Martinez", "Hernandez", "Kim"
+]
+
+random_names = [f"{random.choice(first_names)} {random.choice(last_names)}" for _ in range(100)]
+random_names_str = ", ".join(random_names)
+
+# Convert the list to a single string
+with open("names/unsorted.txt", "w") as f:
+    
+    f.write(random_names_str)
+
