881d5073d97c1a766267b2e48747fa79a5044b01
mr.mitternacht <jamison.proctor@gmail.com>
2023-10-25 10:32:28 +0200

    add linear_search_ex2.py for linear search of names.

---
+++ linear_search_ex2.py
@@ -0,0 +1,14 @@
+import sys
+from load import load_strings
+
+names = load_strings(sys.argv[1])
+
+def index_of_item(collection, target):
+    for i in range(0, len(collection)):
+        if target == collection[i]:
+            return i
+    return None
+
+for n in names:
+    index = index_of_item(names, n)
+    print(index)
