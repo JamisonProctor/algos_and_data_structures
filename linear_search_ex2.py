843e919aeb7cb2fbe9093db2d7dac18183d2b756
mr.mitternacht <jamison.proctor@gmail.com>
2023-10-25 10:58:52 +0200

    add list of names to search into linear_saearch_ex2.py for benchmarking against binary_search_strings.py

--- linear_search_ex2.py
+++ linear_search_ex2.py
@@ -3,12 +3,14 @@ from load import load_strings
 
 names = load_strings(sys.argv[1])
 
+search_names = ["Alice Adams", "Zelda Young"]
+
 def index_of_item(collection, target):
     for i in range(0, len(collection)):
         if target == collection[i]:
             return i
     return None
 
-for n in names:
+for n in search_names:
     index = index_of_item(names, n)
-    print(index)
+    #print(index)
